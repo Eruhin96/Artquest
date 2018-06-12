179e11d3ab88ff118ced68d5d2049fca37e16407
lloydnyarko <l.nyarko@outlook.com>
2018-06-12 20:20:51 +0200

    quick fix solution input

--- app v1/main.js
+++ app v1/main.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-	$('div.solution div.entry input').on('keydown', function(){
-		$('div.solution div.hint').css('display','none');
-		$('div.solution div.submit').css('display','unset');
-		$('div.solution').css('backgroundColor', 'transparent');
-		$('div.solution div.entry').css('backgroundColor', 'rgba(255,255,255,0.8)');
-		});
-
 	$('div.solution div.entry input').on('focus', function(){
-		$('div.solution div.hint').css('display','none');
-		$('div.solution div.submit').css('display','unset');
-		$('div.solution').css('backgroundColor', 'transparent');
-		$('div.solution div.entry').css('backgroundColor', 'rgba(255,255,255,0.8)');
-		});
-	
-	if($('div.solution div.entry input').val().length() == 0){
-		$('div.solution div.entry input').on('focusout', function(){
+		if($('div.solution div.entry input').val().length == 0){
 			$('div.solution div.hint').css('display','unset');
 			$('div.solution div.submit').css('display','none');
 			$('div.solution').css('backgroundColor', 'rgba(255,255,255,0.8)');
 			$('div.solution div.entry').css('backgroundColor', 'transparent');
-			});
-		};
-	
+			}
+
+		else{
+			$('div.solution div.hint').css('display','none');
+			$('div.solution div.submit').css('display','unset');
+			$('div.solution').css('backgroundColor', 'transparent');
+			$('div.solution div.entry').css('backgroundColor', 'rgba(255,255,255,0.8)');
+			}
+		});
+
+	$('div.solution div.entry input').on('keyup', function(){
+		if($('div.solution div.entry input').val().length == 0){
+			$('div.solution div.hint').css('display','unset');
+			$('div.solution div.submit').css('display','none');
+			$('div.solution').css('backgroundColor', 'rgba(255,255,255,0.8)');
+			$('div.solution div.entry').css('backgroundColor', 'transparent');
+			$('div.menu div#tally p').html($('div.solution div.entry input').val().length);
+			}
+
+		else {
+			$('div.solution div.hint').css('display','none');
+			$('div.solution div.submit').css('display','unset');
+			$('div.solution').css('backgroundColor', 'transparent');
+			$('div.solution div.entry').css('backgroundColor', 'rgba(255,255,255,0.8)');
+			$('div.menu div#tally p').html($('div.solution div.entry input').val().length);
+			}
+		});
+	});
