ae7173f80aed84b9d6c69848da504c09e349273e
lloydnyarko <l.nyarko@outlook.com>
2018-06-18 16:44:44 +0200

    navigation

--- app v1/artquest.js
+++ app v1/artquest.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 			$('div.solution div.submit').css('display','none');
 			$('div.solution').css('backgroundColor', 'rgba(255,255,255,0.8)');
 			$('div.solution div.entry').css('backgroundColor', 'transparent');
-			$('div.menu div#tally p').html($(this).val().length);
+			//$('div.menu div#tally p').html($(this).val().length);
 			}
 
 		else {
@@ -39,7 +39,7 @@ $(document).ready(function(){
 			$('div.solution div.submit').css('display','unset');
 			$('div.solution').css('backgroundColor', 'transparent');
 			$('div.solution div.entry').css('backgroundColor', 'rgba(255,255,255,0.8)');
-			$('div.menu div#tally p').html($(this).val().length);
+			//$('div.menu div#tally p').html($(this).val().length);
 
 			$(this).on('keyup', function (e) {
 			    if(e.keyCode  == 13){
@@ -97,8 +97,23 @@ $(document).ready(function(){
 		$(this).prev('div.container.hidden').removeClass('hidden');
 		$(this).addClass('hidden');
 
-		$('div.background.list').not($(this).parent('div.background.list')).css('display','none');
+		$('div.background.list').not($(this).parent('div.background.list')).addClass('hidden');
 		$('div.level').css('display', 'none');
+
+		$('div.menu div p#score').addClass('hidden');
+		$('div.menu div p#back').removeClass('hidden');
+		});
+
+	$('div.menu div p#back').click(function(){
+		$('div.background').addClass('list');
+		$('div.container').addClass('hidden');
+		$('div.background div.list-view').removeClass('hidden');
+
+		$('div.background.list.hidden').removeClass('hidden');
+		$('div.level').css('display', 'unset');
+
+		$('div.menu div p#score').removeClass('hidden');
+		$('div.menu div p#back').addClass('hidden');
 		});
 
 	$('div.menu div svg, div.menu div svg~p' ).click(function(){
